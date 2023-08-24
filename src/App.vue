cdeef9ea8450125521de37629fb500a5c05a2486
masterkudi <cudini.andrea@gmail.com>
2023-08-25 00:14:25 +0200

    impostazione

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  },
+  data() {
+    return {
+      charactersList
+    };
+  },
+};
+</script>
+
+<template>
+
+  <TheHeader></TheHeader>
+
+  <main>
+
+  </main>
+
+  <TheFooter></TheFooter>
+
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+
+}
+
+</script>
+
+<template>
+    <header>
+        <img src="https://www.nicepng.com/png/detail/71-713842_transparent-yugioh-logo-yu-gi-oh-the-art.png" alt="Transparent Yugioh Logo - Yu-gi-oh! The Art Of The Cards@nicepng.com">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore recusandae iste maiores deleniti asperiores id ut obcaecati necessitatibus. Sequi consequuntur nihil illum cumque assumenda, aperiam at veniam sint ipsam illo?
+    </header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
